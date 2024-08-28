584caa47919f13b6fba766333494e8118f1534dc
5940rr <5940rr@gmail.com>
2024-08-28 14:47:19 +0800

    add rich_menu in googlemap_funhtion add user_id in googlemap_funhtion

--- googlemap_funhtion.py
+++ googlemap_funhtion.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, abort
 
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, TextMessage, LocationMessage, TextSendMessage ,FlexSendMessage
+from linebot.models import MessageEvent, TextMessage, LocationMessage, TextSendMessage ,FlexSendMessage, RichMenu, RichMenuArea, RichMenuBounds, RichMenuSize, PostbackAction, PostbackEvent
 from linebot.models.flex_message import BubbleContainer, TextComponent, BoxComponent
 from linebot.exceptions import InvalidSignatureError
 
@@ -13,7 +13,7 @@ import line_flex
 
 app = Flask(__name__)
 
-# telephone = "tel:+123456789"
+user_states = {} # 使用者的id
 
 def get_photo_url(photo_reference, max_width=400):
     """构建照片请求 URL"""
@@ -60,69 +60,115 @@ def callback():
     return 'OK'
 
 #==============================================================
+# 上传 Rich Menu 图片的函数
+def upload_rich_menu_image(line_bot_api, rich_menu_id, image_path):
+    with open('static/rich_menu.jpeg', 'rb') as f:
+        line_bot_api.set_rich_menu_image(rich_menu_id, "image/jpeg", f)
 
-#處理文字訊息
+def create_rich_menu():
+    # 创建主菜单
+    rich_menu_to_create = RichMenu(
+        size=RichMenuSize(width=2500, height=1686),
+        selected=False,
+        name="Main Menu",
+        chat_bar_text="請點擊開始使用隨食即行",
+        areas=[
+            RichMenuArea(
+                bounds=RichMenuBounds(x=59, y=34, width=1140, height=827),
+                action=PostbackAction(data="action=first_layer_url")
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(x=1284, y=8, width=1191, height=853),
+                action=PostbackAction(data="action=first_layer_location")
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(x=63, y=874, width=1136, height=794),
+                action=PostbackAction(data="action=first_layer_location")
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(x=1288, y=874, width=1187, height=807),
+                action=PostbackAction(data="action=location_option1")
+            ),
+        ]
+    )
+
+    rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+
+    # 上传并设置图片，使用您上传的图片路径
+    upload_rich_menu_image(line_bot_api, rich_menu_id, 'static/rich_menu.jpeg')
+
+    # 设置为默认的 Rich Menu
+    line_bot_api.set_default_rich_menu(rich_menu_id)
+
+    print(f"Rich menu created and set as default: {rich_menu_id}")
+#==============================================================
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    data = event.postback.data
+    #if data == "action=first_layer_text":
+        #flex_message = FlexSendMessage(alt_text="地區選擇", contents=locations_flexmessage())
+        #line_bot_api.reply_message(event.reply_token, flex_message)
+    if data == "action=first_layer_url":
+        flex_message = FlexSendMessage(alt_text="米飯類選擇", contents=rice_class())
+        line_bot_api.reply_message(event.reply_token, flex_message)
+        text = "請點選圖卡告訴我你想吃甚麼飯"
+        text_message = TextSendMessage(text)
+        line_bot_api.push_message(event.source.user_id, text_message)
+        
+    elif data == "action=first_layer_location":
+        flex_message = FlexSendMessage(alt_text="麵類選擇", contents=noodle_class())
+        line_bot_api.reply_message(event.reply_token, flex_message)
+        text = "請點選圖卡告訴我你想吃甚麼麵"
+        text_message = TextSendMessage(text)
+        line_bot_api.push_message(event.source.user_id, text_message)
+
+    elif data == "action=location_option1":
+        flex_message = FlexSendMessage(alt_text="甜點選擇", contents=dessert_class())
+        line_bot_api.reply_message(event.reply_token, flex_message)
+        text = "請點選圖卡告訴我你想吃甚麼點心"
+        text_message = TextSendMessage(text)
+        line_bot_api.push_message(event.source.user_id, text_message)
+
+    elif data == "action=location_option2":
+        flex_message = FlexSendMessage(alt_text="異國料理選擇", contents=exotic_cuisine_class())
+        line_bot_api.reply_message(event.reply_token, flex_message)
+        text = "請點選圖卡告訴我你想吃甚麼風格的料理"
+        text_message = TextSendMessage(text)
+        line_bot_api.push_message(event.source.user_id, text_message)
+    
+#==============================================================
+#處理位置訊息
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    global user_states
+    user_id = event.source.user_id
+    location = event.message
+    need_food = user_states.get(user_id, '')  # 獲取用戶的偏好
+
+    flex_message = FlexSendMessage(
+        alt_text='This is a Flex Message',
+        contents=get_store_info(location, need_food)
+    )
+    line_bot_api.reply_message(event.reply_token, flex_message)
+
+#==============================================================
+# 處理文字訊息
 @handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # reply_text = TextSendMessage(text=event.message.text)
-
-    # # 使用 reply_message 方法回應使用者
-    # line_bot_api.reply_message(event.reply_token, reply_text)
-
-    if event.message.text == "飯類":
-        flex_message = FlexSendMessage(
-        alt_text='This is a Flex Message',
-        contents= rice_class()
-        )
-        line_bot_api.reply_message(event.reply_token, flex_message)
-        
-
-    elif event.message.text == "麵類":
-        flex_message = FlexSendMessage(
-        alt_text='This is a Flex Message',
-        contents= noodle_class()
-        )
-        line_bot_api.reply_message(event.reply_token, flex_message)
-
-    elif event.message.text == "點心":
-        flex_message = FlexSendMessage(
-        alt_text='This is a Flex Message',
-        contents= dessert_class()
-        )
-        line_bot_api.reply_message(event.reply_token, flex_message)
-
-    elif event.message.text == "異國料理":
-        flex_message = FlexSendMessage(
-        alt_text='This is a Flex Message',
-        contents= exotic_cuisine_class()
-        )
-        line_bot_api.reply_message(event.reply_token, flex_message)   
-        
-    else:
-        reply_text = TextSendMessage(text='輸入錯誤')
-
-        # 使用 reply_message 方法回應使用者
-        line_bot_api.reply_message(event.reply_token, reply_text)
+def handle_text_message(event):
+    global user_states
+    user_id = event.source.user_id
+    user_states[user_id] = event.message.text  # 保存用戶的偏好
+    text = "請傳送您的定位資訊"
+    text_message = TextSendMessage(text)
+    line_bot_api.reply_message(event.reply_token, text_message)
 
 #==============================================================
 
-#處理位置訊息
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_message(event):
-    location = event.message
-
-    flex_message = FlexSendMessage(
-    alt_text='This is a Flex Message',
-    contents= get_store_info(location)
-    )
-    line_bot_api.reply_message(event.reply_token, flex_message)
-# ==============================================================
-
-def get_store_info(location, max_results=10):
+def get_store_info(location, need_food, max_results=10):
     # Geocoding an address
     origin_location = {'lat':location.latitude, 'lng':location.longitude}
     # 使用 Places API 搜尋附近500公尺內的餐廳
-    places_result = gmaps.places_nearby(location=origin_location, radius=500, keyword='滷肉飯', language="zh-TW")
+    places_result = gmaps.places_nearby(location=origin_location, radius=500, keyword= need_food, language="zh-TW")
 
     places_locations = []
     for place in places_result['results']:
@@ -148,8 +194,6 @@ def get_store_info(location, max_results=10):
         googlemap_url = places_result["result"]['url'] 
         business_time = opening_hours.get('open_now', '無營業時間')
         telephone = 'tel:' + places_result["result"].get("formatted_phone_number", "0000").replace(" ", "")
-        print(f"======================{telephone}=======================")
-        print(f"======================{business_time}=======================")
         
 
         if business_time == True:
@@ -187,4 +231,5 @@ def get_store_info(location, max_results=10):
 
 
 if __name__ == "__main__":
+    create_rich_menu()
     app.run(debug=True)
